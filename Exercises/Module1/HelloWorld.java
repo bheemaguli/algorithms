05bc6341c19ad943b49dc6ad332e4e0cecd293bc
Sumanth Kashyap <sumanth.bhi@gmail.com>
2024-10-22 23:05:49 +0530

    [FIX] HelloWorld from coursera-algorithms-part-1

--- Exercises/Module1/HelloWorld.java
+++ Exercises/Module1/HelloWorld.java
@@ -8,6 +8,6 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        StdOut.println("Hello World");
+        StdOut.println("Hello, World");
     }
 }
